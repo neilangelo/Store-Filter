b295c3e2dd831448dd0277b7cd49785c4ceac36c
neilangelo <neilangelo1223@gmail.com>
2020-06-15 20:24:36 +0800

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,75 @@
+// var myList = ['apples', 'oranges', 'bananas'];
+
+// for ( var i = 0; i < myList.length; i++){
+//     console.log(myList[i]);
+// var sum = 0;
+// var i = [5, 5, 15];
+// i.forEach(numbers);
+
+// function numbers(item) {
+//   sum += item;
+//   console.log(sum);
+//   document.getElementById("demo").innerHTML = sum;
+// }
+
+// Output: 14
+
+// var story = "Start. ";
+// story += "End.";
+// Console.WriteLine(story);
+// // Output: Start. End.
+
+// var fruits, text, fLen, i;
+// fruits = ["Banana", "Oranges"];
+
+// text = "";
+// for (i = 0; i < fruits.length; i++) {
+//   text += fruits[i] + "<br>";
+//   console.log(text);
+// }
+// document.getElementById("demo").innerHTML = text;
+
+// let i = 4;
+// let result = "";
+// while (i > 0) {
+//   result += i;
+//   console.log(result);
+//   i--;
+//   //
+// }
+
+// let person = {
+//   name: "Jhon",
+//   age: 32,
+//   partTime: false,
+// };
+
+// console.log(person);
+
+let person = {
+  name: "Jhon",
+  age: 32,
+  partTime: false,
+};
+
+function incrementAge(person) {
+  console.log(p);
+  person.age++;
+  console.log(p);
+}
+
+incrementAge(person);
+console.log(person.age);
+
+let message = "Hello";
+
+function changeMessage(message) {
+  message = "hi";
+  return;
+}
+
+changeMessage(message);
+
+console.log(message);
+
+
